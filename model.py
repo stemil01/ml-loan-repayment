1e0a434786e0ba70975fc11ea4673e0233bc04ac
stemil01 <s.milenkovic@protonmail.com>
2026-07-17 14:15:43 +0200

    Add FCNN hyperparameter search.

--- model.py
+++ model.py
@@ -1,17 +1,19 @@
 import torch.nn as nn
 
 class NNBinaryClassifier(nn.Module):
-    def __init__(self, in_features, dropout=0.0):
+    def __init__(self, in_features, hidden_dims=(128, 64), dropout=0.0):
         super().__init__()
-        self.seq = nn.Sequential(
-            nn.Linear(in_features=in_features, out_features=128),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Linear(in_features=128, out_features=64),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Linear(in_features=64, out_features=1)
-        )
+
+        layers = []
+        prev = in_features
+        for h in hidden_dims:
+            layers.append(nn.Linear(in_features=prev, out_features=h))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(p=dropout))
+            prev = h
+
+        layers.append(nn.Linear(in_features=prev, out_features=1))
+        self.seq = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.seq(x)
